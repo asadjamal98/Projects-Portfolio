b2fe30a6a8e9e24f3ba67e575c46ee0f2c287071
Asad Jamal <asadjamal98@gmail.com>
2025-07-18 17:32:05 +0200

    test reponse object

--- apis.py
+++ apis.py
@@ -1,1 +1,11 @@
 import requests
+import json
+
+r = requests.get("https://api.github.com//repos/asadjamal98/Projects-Portfolio/commits")
+r.raise_for_status
+#print(json.dumps(r.json(), indent=2))
+result = r.json()
+print(len(result))
+for x in result:
+    print(x.get("sha"))
+
